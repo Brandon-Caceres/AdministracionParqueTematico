a2b6c8dd6775ddf78576482b639d8f4715f2a161
Brandon-Caceres <brandon.caceres.c@mail.pucv.cl>
2025-09-25 16:26:03 -0300

    Manejo de horarios

--- src/administracionparquetematico/Parque.java
+++ src/administracionparquetematico/Parque.java
@@ -24,6 +24,10 @@ public class Parque{
         return new HashMap<>(atracciones);
     }
     
+    public Collection<Atraccion> getAtraccionesCollection() {
+        return Collections.unmodifiableCollection(atracciones.values());
+    }
+    
     public void setAtracciones(Map<Integer, Atraccion> atracciones) {
         if (atracciones == null) {
             throw new IllegalArgumentException("El Map de atracciones no puede ser nulo.");
@@ -59,27 +63,4 @@ public class Parque{
             System.out.println(a);
         }
     }
-    
-    
-    
-    //Datos Iniciales
-    /*public void cargarDatosIniciales(){
-        Atraccion atraccion1 = new Atraccion(1, "Montaña Rusa", "Adrenalina", 20);
-        Atraccion atraccion2 = new Atraccion(2, "Carrusel", "Infantil", 15);
-        Atraccion atraccion3 = new Atraccion(3, "Casa del Terror", "Terror", 10);
-        
-        Reserva reserva1 = new Reserva(100, "Montaña Rusa" ,"2025-09-01");
-        reserva1.agregarPersona(new Persona("Ana", 12));
-        reserva1.agregarPersona(new Persona("Luis", 14));
-        
-        Reserva reserva2 = new Reserva(101, "Carrusel", "2025-09-02");
-        reserva2.agregarPersona(new Persona("Marta", 30));
-        
-        atraccion1.agregarReserva(reserva1);
-        atraccion2.agregarReserva(reserva2);
-        
-        this.agregarAtraccion(atraccion1);
-        this.agregarAtraccion(atraccion2);
-        this.agregarAtraccion(atraccion3);
-    }*/
 }
